12c314695ae64e5a1fd67c072f5604ec2433aec2
Riccardo Zancan <2sleepy@pop-os.localdomain>
2022-12-13 22:58:26 +0100

    Code organized and new Update-Correct module created

--- elm/src/Generate.elm
+++ elm/src/Generate.elm
@@ -1,5 +1,5 @@
 module Generate exposing (..)
-
+import Outfit exposing(..)
 import Browser
 import Html exposing (Html, button, div, text, span, input)
 import Html.Events exposing (onClick, onInput)
@@ -101,31 +101,6 @@ type alias Coords = {
     longitude: Float
     }
 
-type alias Outfit = {
-    head: String,
-    torso_1: String,
-    torso_2: String,
-    torso_3: String,
-    legs: String,
-    feet_1: String,
-    feet_2: String
-    }
-
-initOutfit: Outfit
-initOutfit = {
-    head = "",
-    torso_1 = "",
-    torso_2 = "",
-    torso_3 = "",
-    legs = "",
-    feet_1 = "",
-    feet_2 = ""
-    }
-
-
-outfitToString: Outfit -> String
-outfitToString outfit =
-    outfit.head ++ " - " ++ outfit.torso_1 ++ " - " ++ outfit.torso_2 ++ " - " ++ outfit.torso_3 ++ " - "++ outfit.legs ++ " - " ++ outfit.feet_1 ++ " - " ++ outfit.feet_2
 
 -- HTTP API CALLS
 geocode : String -> Cmd Msg
